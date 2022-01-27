f28e56cd7c39f69d7fc450a143366ebcba596151
Oliver Cederborg <hi@olivercederborg.com>
2022-01-27 03:26:36 +0100

    wip: add Projects section

---
+++ app/components/Projects.tsx
@@ -0,0 +1,18 @@
+export default function Projects() {
+	return (
+		<section id='projects' className='pt-32 mb-32 bg-primary-darker'>
+			<article className='container'>
+				<p className='text-primary-brand font-semibold text-xl uppercase'>Projects</p>
+				<h2 className='uppercase md:text-4xl lg:text-5xl font-extrabold mt-6 text-3xl break-words !leading-[1.25]'>
+					Recent work
+				</h2>
+				<p className='text-2xl text-stone-400 mt-10'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint velit dolorem magni veritatis
+					recusandae consectetur soluta reprehenderit non provident fuga dolores, numquam, hic alias sed
+					asperiores assumenda minus laborum tempora.
+				</p>
+			</article>
+			<section className='grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-[2px] mt-32'>projects here</section>
+		</section>
+	)
+}
