06ea07768baa03acd5f887572e5cf2fc33f07806
Oliver Cederborg <hi@olivercederborg.com>
2022-01-27 03:24:06 +0100

    feat: extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
 	content: ['./app/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'primary-dark': '#101214',
+				'primary-darker': '#0C0D0F',
+				'primary-brand': '#987760'
+			},
+			container: {
+				center: true
+			},
+			fontFamily: {
+				syne: ['Syne', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 }
