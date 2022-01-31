cae62f772168045a80e46a16ca774c7e9badd9f8
Oliver Cederborg <hi@olivercederborg.com>
2022-01-31 22:53:09 +0100

    feat: add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
 			colors: {
 				'primary-dark': '#101214',
 				'primary-darker': '#0C0D0F',
-				'primary-brand': '#987760'
+				'primary-brand': '#987760',
+				oliver: {
+					100: '#d1d1d1',
+					200: '#a2a2a2',
+					300: '#747474',
+					400: '#454545',
+					500: '#171717',
+					600: '#121212',
+					700: '#0e0e0e',
+					800: '#090909',
+					900: '#050505'
+				}
 			},
 			container: {
 				center: true
