b1b95376e0c8dee78c16156b5217a2d49c8b6b30
Oliver Cederborg <hi@olivercederborg.com>
2022-02-02 13:03:22 +0100

    feat: add new Logo to nav + theme switcher

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,17 +1,21 @@
 import ScrollSpy from 'react-scrollspy'
 import { Link } from 'remix'
+import { useTheme } from '~/hooks/useTheme'
+import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md'
+import { Logo } from '~/components/Logo'
 
 export default function Nav() {
+	const { isDarkMode, toggleTheme } = useTheme()
 	return (
 		<nav className='fixed inset-x-0 top-0 z-50 w-full h-32'>
 			<section className='container flex items-center justify-between h-full'>
 				<Link to='/#hero' tabIndex={1}>
-					<img src='/assets/logo.svg' alt='Oliver Cederborg logo' width='56' />
+					<Logo height={36} />
 				</Link>
 				<ScrollSpy
 					items={['hero', 'projects', 'about', 'contact']}
 					currentClassName='active-nav-link'
-					className='gap-x-6 flex text-lg'
+					className='gap-x-14 flex items-center justify-center text-lg'
 					offset={-256}
 				>
 					<li className='hover:text-primary-brand hidden'>
@@ -31,6 +35,15 @@ export default function Nav() {
 						<Link to='/#contact' tabIndex={0}>
 							Contact
 						</Link>
+					</li>
+					<li className='hover:text-primary-brand nav-link'>
+						<button onClick={toggleTheme}>
+							{isDarkMode ? (
+								<MdOutlineLightMode title='Light mode' size={24} />
+							) : (
+								<MdOutlineDarkMode title='Dark mode' size={24} />
+							)}
+						</button>
 					</li>
 				</ScrollSpy>
 			</section>
