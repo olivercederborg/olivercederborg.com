e475111c2434e47eafa3c9d90be81a006f05e630
Oliver Cederborg <hi@olivercederborg.com>
2022-02-02 23:01:01 +0100

    feat: add desktop hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,23 +1,28 @@
+import { VscArrowRight } from 'react-icons/vsc'
 import { Link } from 'remix'
 
 export default function Hero() {
 	return (
-		<header id='hero' className='pt-72 container flex flex-col justify-center px-4 pb-64'>
-			<article>
-				<h1 className='md:text-5xl lg:text-7xl mt-10 text-4xl font-semibold !leading-snug break-words'>
-					<div className='text-primary-brand mb-8 text-4xl font-medium'>Hi, I&apos;m Oliver </div>I design and
-					develop applications.
-				</h1>
-				<p className='text-stone-400 mt-10 text-2xl font-light'>
-					I&apos;m a full-time front-end developer with a passion for great design.
+		<header
+			id='intro'
+			className='pt-96 bg-light-hero dark:bg-dark-hero pb-80 container flex flex-col justify-center px-4 bg-[105%_20vh] bg-no-repeat'
+		>
+			<article className='xl:max-w-[50%]'>
+				<h1 className='text-7xl font-medium leading-tight'>I design and develop applications.</h1>
+				<p className='text-dark-400 dark:text-dark-200 mt-10 text-lg font-light leading-relaxed'>
+					I&apos;m a full-time front-end developer with a passion for great design and user experiences.
 				</p>
 			</article>
 
 			<Link
 				to='#projects'
-				className='hover:translate-x-4 self-start mt-32 text-4xl font-medium transition-transform duration-150 ease-in-out'
+				className='text-dark-400 dark:text-dark-200 hover:text-dark-500 group flex items-center mt-24 text-3xl font-light transition duration-300 ease-in-out'
 			>
-				See my projects
+				Explore my projects
+				<VscArrowRight
+					size={36}
+					className='text-dark-200 dark:text-dark-400 group-hover:translate-x-1 group-hover:translate-y-1 group-hover:text-dark-300 ml-3 transition duration-300 ease-in-out rotate-45'
+				/>
 			</Link>
 		</header>
 	)
