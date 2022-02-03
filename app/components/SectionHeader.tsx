9d7c851562532882e33a076973d177f803f4793a
Oliver Cederborg <hi@olivercederborg.com>
2022-02-03 10:21:25 +0100

    feat: add color transition to SectionHeader

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -8,7 +8,7 @@ export const SectionHeader = forwardRef<HTMLElement, Props>(({ heading, ...props
 	return (
 		<header ref={ref} {...props} className='flex items-center'>
 			<h2 className='text-dark-200 dark:text-dark-400 w-24 text-sm tracking-wider uppercase'>{heading}</h2>
-			<div className='flex-1 h-[1px] bg-dark-100 dark:bg-dark-700' />
+			<div className='flex-1 h-[1px] bg-dark-100 dark:bg-dark-700 transition-colors duration-150 ease-in-out' />
 		</header>
 	)
 })
