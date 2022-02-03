9651a2007328c6d569e018c46e02adb72de2b968
Oliver Cederborg <hi@olivercederborg.com>
2022-02-03 11:32:54 +0100

    feat: update form input to new design

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,58 +1,82 @@
-import { forwardRef, ReactNode } from 'react'
+import clsx from 'clsx'
+import { FC, forwardRef } from 'react'
+import { VscArrowRight } from 'react-icons/vsc'
 import { useField } from 'remix-validated-form'
 
 type InputProps = {
 	name: string
 	label: string
 	placeholder: string
+	required?: boolean
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({ name, label, placeholder }, ref) => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ name, label, placeholder, required }, ref) => {
 	const { error, getInputProps, touched } = useField(name)
 	return (
 		<div>
-			<label htmlFor={name} className='text-primary-brand flex flex-col text-xl'>
+			<label
+				htmlFor={name}
+				className={clsx(
+					'text-dark-400 dark:text-dark-200 inline-flex flex-col text-xl font-light relative ',
+					required && "after:content-['*'] after:absolute after:-right-4 after:text-dark-300"
+				)}
+			>
 				{label}
 			</label>
 			<input
 				ref={ref}
 				{...getInputProps({ id: name, placeholder })}
-				className={`py-4 px-3 bg-transparent text-2xl text-white mt-2 border-b-[3px] border-stone-100/10 placeholder:text-stone-500 w-full focus-within:border-primary-brand outline-none ${
-					error && 'border-red-400'
-				} ${touched && !error && 'border-green-400'}`}
+				className={clsx(
+					'py-4 px-3 bg-transparent text-2xl text-dark-400 dark:text-dark-200 dark:placeholder:text-dark-500 font-light mt-2 border-b-[1px] border-dark-200 dark:border-dark-600 placeholder:text-dark-200 w-full focus-within:border-primary-brand outline-none',
+					error && 'border-red-500 dark:border-red-400',
+					touched && !error && 'border-green-500 dark:border-green-400'
+				)}
 			/>
-			{error && <span className='my-error-class block mt-4 text-red-400'>{error}</span>}
+			{error && <span className='dark:text-red-400 block mt-4 font-light text-red-500'>{error}</span>}
 		</div>
 	)
 })
 Input.displayName = 'Input'
 
-export const Textarea = ({ name, label, placeholder }: InputProps) => {
+export const Textarea = ({ name, label, placeholder, required }: InputProps) => {
 	const { error, getInputProps, touched } = useField(name)
 	return (
 		<div>
-			<label htmlFor={name} className='text-primary-brand flex flex-col text-xl'>
+			<label
+				htmlFor={name}
+				className={clsx(
+					'text-dark-400 dark:text-dark-200 inline-flex flex-col text-xl font-light relative ',
+					required && "after:content-['*'] after:absolute after:-right-4 after:text-dark-300"
+				)}
+			>
 				{label}
 			</label>
 			<textarea
 				{...getInputProps({ id: name, placeholder })}
-				className={`py-4 px-3 bg-transparent text-2xl text-white mt-2 border-b-[3px] border-stone-100/10 placeholder:text-stone-500 w-full focus-within:border-primary-brand outline-none min-h-[10rem] ${
-					error && 'border-red-400'
-				} ${touched && !error && 'border-green-400'}`}
+				className={clsx(
+					'py-4 px-3 h-40 bg-transparent text-2xl text-dark-400 dark:text-dark-200 dark:placeholder:text-dark-500 font-light mt-2 border-b-[1px] border-dark-200 dark:border-dark-600 placeholder:text-dark-200 w-full focus-within:border-primary-brand outline-none',
+					error && 'border-red-500 dark:border-red-400',
+					touched && !error && 'border-green-500 dark:border-green-400'
+				)}
 			></textarea>
-			{error && <span className='my-error-class block mt-4 text-red-400'>{error}</span>}
+			{error && <span className='dark:text-red-400 block mt-4 font-light text-red-500'>{error}</span>}
 		</div>
 	)
 }
+Textarea.displayName = 'Textarea'
 
-export const SubmitButton = ({ children, ...props }: { children: ReactNode }) => {
+export const SubmitButton: FC = ({ children, ...props }) => {
 	return (
 		<button
 			type='submit'
-			className='disabled:text-stone-500 hover:text-primary-brand text-2xl font-semibold uppercase'
+			className='text-dark-400 dark:text-dark-200 hover:text-dark-500 group gap-x-2 flex items-center text-3xl font-light transition duration-300 ease-in-out'
 			{...props}
 		>
 			{children}
+			<VscArrowRight
+				size={36}
+				className='text-dark-200 dark:text-dark-400 group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-dark-300 block mb-1 transition duration-300 ease-in-out -rotate-45'
+			/>
 		</button>
 	)
 }
