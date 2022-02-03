dd7c76d24fda98b97ed315a758715b4590d91d9a
Oliver Cederborg <hi@olivercederborg.com>
2022-02-03 20:26:53 +0100

    feat: decrease phone padding

--- app/components/SectionShell.tsx
+++ app/components/SectionShell.tsx
@@ -3,7 +3,7 @@ import { ComponentPropsWithoutRef, FC } from 'react'
 type SectionShellProps = ComponentPropsWithoutRef<'section'>
 
 export const SectionShell: FC<SectionShellProps> = ({ children, ...props }) => (
-	<section className='container pb-64' {...props}>
+	<section className='md:pb-64 container pb-40' {...props}>
 		{children}
 	</section>
 )
