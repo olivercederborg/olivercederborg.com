41f08a8fb10fe0f874cb95e2f487f189b21d7401
Oliver Cederborg <hi@olivercederborg.com>
2022-02-03 10:21:47 +0100

    feat: clean up Nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,52 +1,48 @@
 import ScrollSpy from 'react-scrollspy'
-import { Link } from 'remix'
 import { useTheme } from '~/hooks/useTheme'
 import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md'
 import { Logo } from '~/components/Logo'
+import { Link } from 'remix'
 
 export default function Nav() {
 	const { theme, toggleTheme } = useTheme()
 	return (
-		<nav className='fixed inset-x-0 top-0 z-50 w-full h-32'>
-			<section className='container flex items-center justify-between h-full'>
-				<Link to='/#hero' tabIndex={1}>
-					<Logo height={36} />
+		<nav className='container fixed inset-x-0 top-0 z-50 flex items-center justify-between w-full h-32'>
+			<Link to='/#hero' tabIndex={0}>
+				<Logo height={36} />
+			</Link>
+
+			<ScrollSpy
+				items={['intro', 'projects', 'about', 'contact']}
+				currentClassName='active-nav-link'
+				className='gap-x-14 flex items-center justify-center text-lg'
+				componentTag='div'
+				offset={-256}
+			>
+				<Link to='/#intro' className='hover:text-primary-brand nav-link'>
+					Introduction
 				</Link>
-				<ScrollSpy
-					items={['hero', 'projects', 'about', 'contact']}
-					currentClassName='active-nav-link'
-					className='gap-x-14 flex items-center justify-center text-lg'
-					offset={-256}
+				<Link to='/#projects' className='hover:text-primary-brand nav-link'>
+					Projects
+				</Link>
+				<Link to='/#about' className='hover:text-primary-brand nav-link'>
+					About
+				</Link>
+				<Link to='/#contact' className='hover:text-primary-brand nav-link'>
+					Contact
+				</Link>
+
+				<button
+					className='hover:text-primary-brand text-dark-400 dark:text-dark-300 hover:text-dark-500 dark:hover:text-dark-200'
+					onClick={toggleTheme}
 				>
-					<li className='hover:text-primary-brand hidden'>
-						<Link to='/#hero'>Hero</Link>
-					</li>
-					<li className='hover:text-primary-brand nav-link'>
-						<Link to='/#projects' tabIndex={0}>
-							Projects
-						</Link>
-					</li>
-					<li className='hover:text-primary-brand nav-link'>
-						<Link to='/#about' tabIndex={0}>
-							About
-						</Link>
-					</li>
-					<li className='hover:text-primary-brand nav-link'>
-						<Link to='/#contact' tabIndex={0}>
-							Contact
-						</Link>
-					</li>
-					<li className='hover:text-primary-brand nav-link'>
-						<button onClick={toggleTheme}>
-							{theme === 'dark' ? (
-								<MdOutlineLightMode title='Light mode' size={24} />
-							) : (
-								<MdOutlineDarkMode title='Dark mode' size={24} />
-							)}
-						</button>
-					</li>
-				</ScrollSpy>
-			</section>
+					{theme === 'dark' ? (
+						<MdOutlineLightMode title='Light mode' size={24} />
+					) : (
+						<MdOutlineDarkMode title='Dark mode' size={24} />
+					)}
+				</button>
+			</ScrollSpy>
 		</nav>
 	)
 }
