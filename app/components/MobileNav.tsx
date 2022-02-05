20185169bd5e879078557fd164a025e78cbd91c7
Oliver Cederborg <hi@olivercederborg.com>
2022-02-05 02:32:35 +0100

    feat: update menu toggle

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -69,14 +69,14 @@ export default function MobileNav() {
 				type='button'
 				aria-label={isOpen ? 'Close menu' : 'Open menu'}
 				onClick={toggle}
-				className='md:hidden'
+				className='md:hidden top-8 right-4 hover:opacity-60 mix-blend-difference dark:text-white text-dark-100 fixed z-30'
 			>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					fill='none'
 					viewBox='0 0 24 24'
 					stroke='currentColor'
-					className='top-8 right-4 fixed z-30 w-8 h-8'
+					className='w-8 h-8'
 				>
 					<AnimatePresence exitBeforeEnter>
 						{isOpen ? (
@@ -87,7 +87,7 @@ export default function MobileNav() {
 								exit={{ pathOffset: 1 }}
 								transition={{ duration: 1, ease: 'circOut' }}
 								strokeLinecap='square'
-								strokeWidth={2}
+								strokeWidth={1.5}
 								d='M6 18L18 6M6 6l12 12'
 							/>
 						) : (
@@ -96,10 +96,10 @@ export default function MobileNav() {
 								initial={{ pathLength: 0 }}
 								animate={{ pathLength: 1, pathOffset: 0 }}
 								exit={{ pathOffset: 1 }}
-								transition={{ duration: 0.5, ease: 'circOut' }}
+								transition={{ duration: 1, ease: 'circOut' }}
 								strokeLinecap='square'
-								strokeWidth={2}
-								d='M4 8h16M4 16h16'
+								strokeWidth={1.5}
+								d='M4 6h16M4 12h16m-7 6h7'
 							/>
 						)}
 					</AnimatePresence>
@@ -114,41 +114,25 @@ export default function MobileNav() {
 						initial='hidden'
 						animate='visible'
 						exit='hidden'
-						className='md:hidden bg-dark-200 dark:bg-dark-700 container fixed top-0 right-0 z-20 flex flex-col items-center justify-between w-9/12 h-full'
+						className='md:hidden bg-dark-100 dark:bg-dark-700 fixed inset-y-0 right-0 z-20 flex flex-col justify-between w-9/12 px-12'
 					>
 						<ScrollSpy
 							items={['intro', 'projects', 'about', 'contact']}
 							currentClassName='active-nav-link'
-							className='gap-y-14 flex flex-col items-center justify-center h-full'
+							className='gap-y-14 flex flex-col items-start justify-center h-full'
 							componentTag='div'
 							offset={-256}
 						>
-							<MotionLink
-								to='/#'
-								variants={linkVariants}
-								className='hover:text-primary-brand nav-link text-2xl'
-							>
+							<MotionLink to='/#' variants={linkVariants} className='mobile-nav-link'>
 								Introduction
 							</MotionLink>
-							<MotionLink
-								to='/#projects'
-								variants={linkVariants}
-								className='hover:text-primary-brand nav-link text-2xl'
-							>
+							<MotionLink to='/#projects' variants={linkVariants} className='mobile-nav-link'>
 								Projects
 							</MotionLink>
-							<MotionLink
-								to='/#about'
-								variants={linkVariants}
-								className='hover:text-primary-brand nav-link text-2xl'
-							>
+							<MotionLink to='/#about' variants={linkVariants} className='mobile-nav-link'>
 								About
 							</MotionLink>
-							<MotionLink
-								to='/#contact'
-								variants={linkVariants}
-								className='hover:text-primary-brand nav-link text-2xl'
-							>
+							<MotionLink to='/#contact' variants={linkVariants} className='mobile-nav-link'>
 								Contact
 							</MotionLink>
 							<motion.div variants={linkVariants}>
