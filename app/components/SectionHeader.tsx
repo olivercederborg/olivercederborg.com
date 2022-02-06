8b7a11a3b54c8de351654003ace4387e4ddd0194
Oliver Cederborg <hi@olivercederborg.com>
2022-02-06 22:35:09 +0100

    refactor: rename props type

--- app/components/SectionHeader.tsx
+++ app/components/SectionHeader.tsx
@@ -34,7 +34,6 @@ const parentVariants = {
 	visible: {
 		opacity: 1,
 		transition: {
-			// when: 'beforeChildren',
 			staggerChildren: 0.25,
 		},
 	},
@@ -46,32 +45,34 @@ const parentVariants = {
 	},
 }
 
-type Props = {
+type SectionHeaderProps = {
 	heading: string
 } & ComponentPropsWithRef<'header'> &
 	HTMLMotionProps<'header'>
 
-export const SectionHeader = forwardRef<HTMLElement, Props>(({ heading, ...props }, ref) => (
-	<motion.header
-		ref={ref}
-		{...props}
-		className='flex items-center'
-		variants={parentVariants}
-		initial='hidden'
-		whileInView='visible'
-		viewport={{ once: true }}
-	>
-		<motion.h2
-			variants={headerVariants}
-			className='w-24 text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'
+export const SectionHeader = forwardRef<HTMLElement, SectionHeaderProps>(
+	({ heading, ...props }, ref) => (
+		<motion.header
+			ref={ref}
+			{...props}
+			className='flex items-center'
+			variants={parentVariants}
+			initial='hidden'
+			whileInView='visible'
+			viewport={{ once: true }}
 		>
-			{heading}
-		</motion.h2>
-		<motion.div
-			variants={lineVariants}
-			className='h-[1px] flex-1 bg-dark-100 transition-colors duration-150 ease-in-out dark:bg-dark-700'
-		/>
-	</motion.header>
-))
+			<motion.h2
+				variants={headerVariants}
+				className='w-24 text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'
+			>
+				{heading}
+			</motion.h2>
+			<motion.div
+				variants={lineVariants}
+				className='h-[1px] flex-1 bg-dark-100 transition-colors duration-150 ease-in-out dark:bg-dark-700'
+			/>
+		</motion.header>
+	)
+)
 
 SectionHeader.displayName = 'SectionHeader'
