c87ab6af222002656e41b6802dfdf7886deed9dd
Oliver Cederborg <hi@olivercederborg.com>
2022-02-06 22:33:15 +0100

    feat: animate Projects section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,60 +1,96 @@
+import { motion } from 'framer-motion'
 import { VscArrowRight } from 'react-icons/vsc'
-import { Link } from 'remix'
+import { AnimatedText } from '~/components/AnimatedText'
+import { MotionLinkButton } from '~/components/LinkButton'
+import { ProjectItem } from '~/components/ProjectItem'
 import { SectionHeader } from '~/components/SectionHeader'
 import { SectionShell } from '~/components/SectionShell'
+import { projects } from '../../projects'
 
 export default function Projects() {
 	return (
 		<SectionShell id='projects'>
 			<SectionHeader heading='Projects' />
 
-			<section className='relative mt-16 grid grid-cols-12 gap-y-10 md:ml-24 md:gap-x-8 lg:gap-x-16'>
-				<p className='col-span-full text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200 md:col-span-6 xl:col-span-8'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum modi maxime, nobis
-					aperiam at ducimus id praesentium ut itaque eos, suscipit doloremque quaerat deleniti
-					officia accusamus.
-				</p>
-				<Link
+			<motion.section
+				variants={{
+					visible: { transition: { staggerChildren: 0.25 } },
+				}}
+				initial='hidden'
+				whileInView='visible'
+				exit='hidden'
+				viewport={{ once: true }}
+				className='relative mt-16 grid grid-cols-12 gap-y-10 md:ml-24 md:gap-x-8 lg:gap-x-16'
+			>
+				<AnimatedText
+					as='p'
+					className='col-span-full text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200 md:col-span-6 xl:col-span-8'
+					text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum modi maxime, nobis aperiam at ducimus id praesentium ut itaque eos, suscipit doloremque quaerat deleniti officia accusamus.'
+				/>
+
+				<MotionLinkButton
 					to='/projects'
-					className='group col-span-full flex items-center self-start text-3xl font-light text-dark-400 transition duration-300 ease-in-out hover:text-dark-500 dark:text-dark-200 md:col-start-7 xl:col-start-9'
+					motionProps={{
+						variants: {
+							hidden: { opacity: 0, y: 50 },
+							visible: {
+								opacity: 1,
+								y: 0,
+								transition: { ease: 'circOut', duration: 0.5 },
+							},
+						},
+					}}
+					className='col-span-full md:col-start-7 xl:col-start-9'
 				>
 					All projects
-					<VscArrowRight
-						size={36}
-						className='ml-3 rotate-45 text-dark-200 transition duration-300 ease-in-out group-hover:translate-x-1 group-hover:translate-y-1 group-hover:text-dark-300 dark:text-dark-400'
-					/>
-				</Link>
-			</section>
+				</MotionLinkButton>
+			</motion.section>
 
-			<div className='mt-24 flex'>
-				<Link
+			<motion.div
+				variants={{
+					visible: { transition: { staggerChildren: 0.25 } },
+				}}
+				initial='hidden'
+				whileInView='visible'
+				exit='hidden'
+				viewport={{ once: true }}
+				className='mt-24 flex'
+			>
+				<MotionLinkButton
 					to='/side-projects'
+					motionProps={{
+						variants: {
+							hidden: { opacity: 0, x: -50 },
+							visible: {
+								opacity: 1,
+								x: 0,
+								transition: { ease: 'circOut', duration: 0.5 },
+							},
+						},
+					}}
 					className='group flex w-14 items-end gap-3 self-start text-2xl font-light text-dark-400 transition duration-300 ease-in-out hover:text-dark-500 dark:text-dark-200 md:w-24 md:text-3xl'
 					style={{ writingMode: 'vertical-rl' }}
+					icon={
+						<VscArrowRight className='rotate-[135deg] text-3xl text-dark-200 transition duration-300 ease-in-out group-hover:-translate-x-1 group-hover:translate-y-1 group-hover:text-dark-300 dark:text-dark-400 md:text-4xl' />
+					}
 				>
 					Side projects
-					<VscArrowRight className='rotate-[135deg] text-3xl text-dark-200 transition duration-300 ease-in-out group-hover:-translate-x-1 group-hover:translate-y-1 group-hover:text-dark-300 dark:text-dark-400 md:text-4xl' />
-				</Link>
-				<section className='relative grid flex-1 grid-cols-12 gap-y-12 md:gap-x-8 md:gap-y-16 lg:gap-x-16'>
-					{[...Array(3).keys()].map(t => (
-						<Link
-							to='/projects/miinto'
-							key={t}
-							className='col-span-12 flex flex-col md:col-span-6 xl:col-span-4'
-						>
-							<img
-								src='https://cdn.dribbble.com/users/1858541/screenshots/17394216/media/113b3233d9f61e804377a2aa2488d613.png'
-								alt='Miinto 2.0 app design'
-								className='object-cover'
-							/>
-							<p className='mt-6 text-sm font-light text-dark-300 dark:text-dark-400'>
-								Design and Development
-							</p>
-							<h3 className='mt-3 text-3xl text-dark-400 dark:text-dark-200'>Miinto 2.0</h3>
-						</Link>
+				</MotionLinkButton>
+				<motion.section
+					variants={{
+						visible: { transition: { staggerChildren: 0.25 } },
+					}}
+					initial='hidden'
+					whileInView='visible'
+					exit='hidden'
+					viewport={{ once: true }}
+					className='relative grid flex-1 grid-cols-12 gap-y-12 md:gap-x-8 md:gap-y-16 lg:gap-x-16'
+				>
+					{projects.map(t => (
+						<ProjectItem key={t.name} project={t} />
 					))}
-				</section>
-			</div>
+				</motion.section>
+			</motion.div>
 		</SectionShell>
 	)
 }
