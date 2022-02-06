779d26b94e85e5216312a8cb81eafa9eca1d0ca7
Oliver Cederborg <hi@olivercederborg.com>
2022-02-06 23:03:05 +0100

    feat: add animation to About section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,14 +1,15 @@
-import { VscArrowRight } from 'react-icons/vsc'
-import { Link } from 'remix'
+import { motion } from 'framer-motion'
 import { IoLogoFigma } from 'react-icons/io5'
 import {
-	SiVim,
+	SiNextdotjs,
+	SiReact,
 	SiTailwindcss,
 	SiTypescript,
-	SiReact,
+	SiVim,
 	SiVuedotjs,
-	SiNextdotjs,
 } from 'react-icons/si'
+import { AnimatedText } from '~/components/AnimatedText'
+import { MotionLinkButton } from '~/components/LinkButton'
 import { SectionHeader } from '~/components/SectionHeader'
 import { SectionShell } from '~/components/SectionShell'
 
@@ -17,52 +18,91 @@ export default function About() {
 		<SectionShell id='about'>
 			<SectionHeader heading='About' />
 
-			<section className='relative mt-16 grid grid-cols-12 gap-y-10 md:ml-24 md:gap-x-8 lg:gap-x-16'>
-				<p className='col-span-full text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200 md:col-span-6 xl:col-span-8'>
-					Get a brief look at who I am and what I do. <br />
-					If you would like to know more about me and my interests, you can.
-				</p>
-				<Link
-					to='/projects'
-					className='group col-span-full flex items-center self-start text-3xl font-light text-dark-400 transition duration-300 ease-in-out hover:text-dark-500 dark:text-dark-200 md:col-start-7 xl:col-start-9'
+			<motion.section
+				variants={{
+					visible: { transition: { staggerChildren: 0.25 } },
+				}}
+				initial='hidden'
+				whileInView='visible'
+				exit='hidden'
+				viewport={{ once: true }}
+				className='relative mt-16 grid grid-cols-12 gap-y-10 md:ml-24 md:gap-x-8 lg:gap-x-16'
+			>
+				<AnimatedText
+					as='p'
+					className='col-span-full text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200 md:col-span-6 xl:col-span-8'
+					text='Get a brief look at who I am and what I do. If you would like to know more about me and my interests, you can.'
+				/>
+				<MotionLinkButton
+					to='/about'
+					motionProps={{
+						variants: {
+							hidden: { opacity: 0, y: 50 },
+							visible: { opacity: 1, y: 0, transition: { ease: 'circOut', duration: 0.5 } },
+						},
+					}}
+					className='col-span-full md:col-start-7 xl:col-start-9'
 				>
 					More about me
-					<VscArrowRight
-						size={36}
-						className='ml-3 rotate-45 text-dark-200 transition duration-300 ease-in-out group-hover:translate-x-1 group-hover:translate-y-1 group-hover:text-dark-300 dark:text-dark-400'
-					/>
-				</Link>
-			</section>
+				</MotionLinkButton>
+			</motion.section>
 
-			<section className='relative mt-24 flex grid-cols-12 flex-col-reverse gap-y-10 md:ml-24 md:grid md:gap-x-8 lg:gap-x-16'>
+			<motion.section
+				variants={{
+					visible: { transition: { staggerChildren: 0.15, delayChildren: 0.25 } },
+				}}
+				initial='hidden'
+				whileInView='visible'
+				exit='hidden'
+				viewport={{ once: true }}
+				className='relative mt-24 flex grid-cols-12 flex-col-reverse gap-y-10 md:ml-24 md:grid md:gap-x-8 lg:gap-x-16'
+			>
 				<section className='col-span-full md:col-span-6 xl:col-span-8'>
 					<article>
-						<h3 className='w-24 text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'>
-							Who am I
-						</h3>
-						<p className='mt-6 text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200'>
-							My name is Oliver Cederborg. I am 24 years old, I live in Copenhagen, Denmark,
-							and I&apos;m a self-taught designer and developer.
-							<br />
-							<br />
-							Digital design has been my main focus for many years, specifically UI/UX, but I
-							have the past years shifted my focus to front-end development, which I have
-							developed a great passion for.
-							<br />
-							<br />
-							I enjoy the constant change in the technologies used in the area and love
-							diving into new frameworks and technologies.
-							<br />
-							<br />I try to challenge myself by diving head-first into the unknown, learning
-							the skills along the way, needed to complete the task.
-						</p>
+						<AnimatedText
+							as='h3'
+							text='Who am I'
+							className='text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'
+						/>
+						<AnimatedText
+							as='p'
+							text="My name is Oliver Cederborg. I am 24 years old, I live in Copenhagen, Denmark, and I'm a self-taught designer and developer."
+							className='mt-6 text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200'
+						/>
+						<AnimatedText
+							as='p'
+							text='Digital design has been my main focus for many years, specifically UI/UX, but I have the past years shifted my focus to front-end development, which I have developed a great passion for.'
+							className='mt-6 text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200'
+						/>
+						<AnimatedText
+							as='p'
+							text='I enjoy the constant change in the technologies used in the area and love diving into new frameworks and technologies.'
+							className='mt-6 text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200'
+						/>
+						<AnimatedText
+							as='p'
+							text='I try to challenge myself by diving head-first into the unknown, learning the skills along the way, needed to complete the task.'
+							className='mt-6 text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200'
+						/>
 					</article>
 
-					<section className='col-span-full mt-14 md:col-span-6 xl:col-span-8'>
-						<h3 className='w-24 text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'>
-							Tech I enjoy
-						</h3>
-						<div className='mt-6 flex flex-wrap gap-6 text-dark-300'>
+					<section className='col-span-full mt-14 overflow-hidden md:col-span-6 xl:col-span-8'>
+						<AnimatedText
+							as='h3'
+							text='Tech I enjoy'
+							className='text-sm uppercase tracking-wider text-dark-200 dark:text-dark-400'
+						/>
+						<motion.div
+							variants={{
+								hidden: { y: 50, opacity: 0 },
+								visible: {
+									y: 0,
+									opacity: 1,
+									transition: { duration: 0.5, ease: 'circOut' },
+								},
+							}}
+							className='mt-6 flex flex-wrap gap-6 text-dark-300'
+						>
 							<IoLogoFigma size={28} title='Figma' />
 							<SiVim size={28} title='Vim' />
 							<SiTailwindcss size={28} title='TailwindCSS' />
@@ -70,16 +110,41 @@ export default function About() {
 							<SiReact size={28} title='React.js' />
 							<SiVuedotjs size={28} title='Vue.js' />
 							<SiNextdotjs size={28} title='Next.js' />
-						</div>
+						</motion.div>
 					</section>
 				</section>
 
-				<img
-					src='/assets/olivercederborg-portrait.jpg'
-					alt='Portrait of Oliver Cederborg'
-					className='w-2/3 md:col-span-full md:col-start-7 md:w-full xl:col-start-9'
-				/>
-			</section>
+				<motion.figure
+					variants={{
+						hidden: { scaleX: 0, originX: 0 },
+						visible: {
+							scaleX: 1,
+							opacity: 1,
+							transition: {
+								duration: 0.75,
+								ease: [0.9, 0.1, 0.3, 0.96],
+								when: 'beforeChildren',
+								delayChildren: 0.15,
+								delay: 0.25,
+							},
+						},
+					}}
+					style={{ backgroundColor: '#d4d4d4' }}
+					className='mb-6 w-2/3 self-start md:col-span-full md:col-start-7 md:mb-0 md:w-full xl:col-start-9'
+				>
+					<motion.img
+						variants={{
+							hidden: { opacity: 0 },
+							visible: {
+								opacity: 1,
+								transition: { duration: 0.5, ease: [0.6, 0.5, 0.5, 0.9] },
+							},
+						}}
+						src='/assets/olivercederborg-portrait.jpg'
+						alt='Portrait of Oliver Cederborg'
+					/>
+				</motion.figure>
+			</motion.section>
 		</SectionShell>
 	)
 }
