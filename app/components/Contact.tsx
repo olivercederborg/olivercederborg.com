657914d3a7081704024c4c3a9d1d8066b3e58864
Oliver Cederborg <hi@olivercederborg.com>
2022-02-11 23:54:12 +0100

    feat(contact): add motion wrapper

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import type { FC } from 'react'
 import { SectionHeader } from '~/components/SectionHeader'
 import { SectionShell } from '~/components/SectionShell'
@@ -6,7 +7,16 @@ const Contact: FC = ({ children }) => (
 	<SectionShell id='contact'>
 		<SectionHeader heading='Contact' />
 
-		<section className='mt-16 flex flex-1 flex-col gap-x-16 gap-y-24 md:ml-24 md:flex-row'>
+		<motion.section
+			variants={{
+				visible: { transition: { staggerChildren: 0.15, delayChildren: 0.25 } },
+			}}
+			initial='hidden'
+			whileInView='visible'
+			exit='hidden'
+			viewport={{ once: true }}
+			className='mt-16 flex flex-1 flex-col gap-x-16 gap-y-24 md:ml-24 md:flex-row'
+		>
 			<div className='md:w-1/2'>
 				<p className='col-span-full text-lg font-light leading-relaxed text-dark-400 dark:text-dark-200 md:col-span-6 xl:col-span-8'>
 					Do not hesitate to contact me through the form here or by direct email on{' '}
@@ -20,7 +30,7 @@ const Contact: FC = ({ children }) => (
 				</p>
 			</div>
 			<div className='md:w-1/2'>{children}</div>
-		</section>
+		</motion.section>
 	</SectionShell>
 )
 export default Contact
