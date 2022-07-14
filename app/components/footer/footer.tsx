34151bd3b163840e29954ff35e11087cf1587075
Oliver Cederborg <hey@olivercederborg.com>
2022-07-14 19:01:24 +0200

    feat(footer): add github link

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -11,7 +11,16 @@ export const Footer = () => {
       viewport={{ once: true }}
       className='container flex flex-col justify-center gap-10 py-16 md:flex-row md:justify-between'
     >
-      <section className='flex w-1/2 gap-12 text-xl'>
+      <section className='flex w-1/2 gap-8 md:gap-12 text-xl'>
+        <motion.a
+          href='https://github.com/olivercederborg'
+          target='_blank'
+          rel='noreferrer'
+          variants={footerItemVariants}
+          className='nav-link text-xl text-dark-500'
+        >
+          Github
+        </motion.a>
         <motion.a
           href='https://dribbble.com/oliver'
           target='_blank'
