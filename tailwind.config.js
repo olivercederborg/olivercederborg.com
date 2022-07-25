2e713831c9e5c9726f28ec19314f49bed7c686df
Oliver Cederborg <hey@olivercederborg.com>
2022-07-26 01:52:12 +0200

    chore(tailwind): add config typing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,37 @@
 /* eslint-disable unicorn/prefer-module */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./app/**/*.{js,ts,jsx,tsx}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				dark: {
-					50: '#EDEDED',
-					100: '#dadada',
-					200: '#b5b5b5',
-					300: '#8f8f8f',
-					400: '#6a6a6a',
-					500: '#454545',
-					600: '#373737',
-					700: '#292929',
-					800: '#1c1c1c',
-					850: '#171717',
-					900: '#0e0e0e',
-				},
-			},
-			container: {
-				center: true,
-				padding: '1rem',
-			},
-			backgroundImage: {
-				'dark-hero': "url('/assets/dark-hero.png')",
-				'light-hero': "url('/assets/light-hero.png')",
-			},
-			screens: {
-				'2xl': '1440px',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          50: '#EDEDED',
+          100: '#dadada',
+          200: '#b5b5b5',
+          300: '#8f8f8f',
+          400: '#6a6a6a',
+          500: '#454545',
+          600: '#373737',
+          700: '#292929',
+          800: '#1c1c1c',
+          850: '#171717',
+          900: '#0e0e0e',
+        },
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      backgroundImage: {
+        'dark-hero': "url('/assets/dark-hero.png')",
+        'light-hero': "url('/assets/light-hero.png')",
+      },
+      screens: {
+        '2xl': '1440px',
+      },
+    },
+  },
+  plugins: [],
 }
