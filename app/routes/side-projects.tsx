834580aab24ddf22770fb25ba7f87af6e6795f05
Oliver Cederborg <hey@olivercederborg.com>
2022-08-07 04:48:01 +0200

    feat(side-projects): fetch and display github stars

--- app/routes/side-projects.tsx
+++ app/routes/side-projects.tsx
@@ -1,11 +1,10 @@
-import type { MetaFunction } from 'remix'
+import type { HeadersFunction, LoaderFunction, MetaFunction } from 'remix'
+import { useLoaderData } from 'remix'
 
 import { AnimatePresence, motion } from 'framer-motion'
 
 import { AnimatedLetters, AnimatedText } from '~/components/animated-text'
 import { SideProjectItem } from '~/components/side-project-item'
-
-import { projects } from '../../sideprojects'
 
 export const meta: MetaFunction = () => ({
   title: 'Side Projects - Oliver Cederborg',
@@ -13,7 +12,93 @@ export const meta: MetaFunction = () => ({
     "I'm a 25 year old self-taught designer & frontend developer, focused on user experience, accessibility and modern web technologies.",
 })
 
+export const headers: HeadersFunction = () => ({
+  'Cache-Control': 'max-age=600, s-maxage=3600',
+})
+
+export type Sideproject = {
+  id: number
+  name: string
+  area: string
+  url: string
+  image: string
+  imageAlt: string
+  color?: string
+  repo?: string
+  stars?: number
+}
+
+export type LoaderData = {
+  projects: Required<Sideproject[]>
+}
+
+export const loader: LoaderFunction = async () => {
+  const sideProjects: Sideproject[] = [
+    {
+      id: 0,
+      name: 'Poimandres.nvim',
+      area: 'Neovim Plugin',
+      image:
+        'https://user-images.githubusercontent.com/47901349/180445055-92480553-0652-4155-8d41-835fec27245b.png',
+      imageAlt: 'Poimandres Neovim color scheme by Oliver Cederborg',
+      repo: 'olivercederborg/poimandres.nvim',
+      url: 'https://github.com/olivercederborg/poimandres.nvim',
+      color: '#23393D',
+    },
+    {
+      id: 1,
+      name: 'Poimandres-wezterm',
+      area: 'WezTerm Color Scheme',
+      image:
+        'https://user-images.githubusercontent.com/47901349/179416018-ba8e24a6-3590-4eff-93cb-806d41378a0d.png',
+      imageAlt: 'Poimandres WezTerm color scheme by Oliver Cederborg',
+      repo: 'olivercederborg/poimandres-wezterm',
+      url: 'https://github.com/olivercederborg/poimandres-wezterm',
+      color: '#23393D',
+    },
+    {
+      id: 2,
+      name: 'Modern Monokai',
+      area: 'VSCode Color Scheme',
+      image: 'https://github.com/olivercederborg/modern-monokai/raw/master/assets/screen1.png',
+      imageAlt: 'Modern Monokai color scheme by Oliver Cederborg',
+      repo: 'olivercederborg/modern-monokai',
+      url: 'https://marketplace.visualstudio.com/items?itemName=OliverCederborg.modern-monokai',
+      color: '#292E36',
+    },
+  ]
+
+  const projects: Sideproject[] = []
+
+  for await (const project of sideProjects) {
+    if (project.repo) {
+      const data = await fetch(`https://api.github.com/repos/${project.repo}`, {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: `token ${process.env.GITHUB_TOKEN}`,
+        },
+      })
+      const { stargazers_count: stars } = (await data.json()) as unknown as {
+        stargazers_count: number
+      }
+
+      projects.push({
+        ...project,
+        stars,
+      })
+    } else {
+      projects.push(project)
+    }
+  }
+
+  return {
+    projects,
+  }
+}
+
 export default function Sideprojects() {
+  const { projects } = useLoaderData<LoaderData>()
+
   return (
     <main className='container py-40'>
       <motion.article
