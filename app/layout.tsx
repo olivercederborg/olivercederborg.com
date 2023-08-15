f894c8056740aaeaa978704c06f8948983a30d04
Oliver Cederborg <hey@olivercederborg.com>
2023-08-15 20:25:00 +0200

    merge(next): add analytics, static file gen, 404 fix (#30)
    * refactor(not-found): remove shell template, move markup to `not-found.tsx`
    
    * fix(not-found): add temp 404 fallback fix
    
    * feat: add robots/sitemap generation
    
    * feat(analytics): add vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react'
 
 import localFont from 'next/font/local'
 
@@ -45,6 +46,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Navigation />
           <MobileNav />
           {children}
+          <Analytics />
           <Footer />
         </Providers>
       </body>
