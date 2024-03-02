ecfc3cd3cd66cee8634b170f7c8887c21662efca
Oliver Cederborg <hey@olivercederborg.com>
2024-03-02 21:04:44 +0100

    feat(2024): init

--- app/components/section-shell.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentPropsWithoutRef } from 'react'
-
-type SectionShellProps = ComponentPropsWithoutRef<'section'>
-
-export const SectionShell = ({ children, ...props }: SectionShellProps) => (
-  <section className='container pb-40 md:pb-64' {...props}>
-    {children}
-  </section>
-)
--- app/utils/get-error-message.ts
+++
@@ -1,4 +0,0 @@
-export const getErrorMessage = (error: unknown) => {
-  if (error instanceof Error) return error.message
-  return String(error)
-}
