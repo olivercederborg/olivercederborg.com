518774f033fbdf3d4f05985f124e097cb1fa6856
Oliver Cederborg <hey@olivercederborg.com>
2024-03-18 02:52:11 +0100

    feat(guestbook): add light mode styling

--- app/guestbook/entry.tsx
+++ app/guestbook/entry.tsx
@@ -12,15 +12,18 @@ export function Entry({ entry, className }: EntryProps) {
    return (
       <div
          className={cn(
-            "flex flex-col flex-wrap items-start self-start text-pretty break-all rounded-xl border border-white/5 bg-neutral-900/10 px-3 py-2 text-sm leading-6",
+            "flex flex-col flex-wrap items-start self-start text-pretty break-all rounded-xl border bg-neutral-200/10 px-3 py-2 text-sm leading-6 dark:border-white/5 dark:bg-neutral-900/10",
             {
-               "items-end self-end bg-neutral-400/10": isAuthor,
+               "items-end self-end bg-neutral-700/10 dark:bg-neutral-400/10":
+                  isAuthor,
             },
             className,
          )}
       >
          <p className="flex flex-col">
-            <span className="text-xs text-neutral-400">{entry.createdBy}</span>
+            <span className="text-xs text-neutral-500 dark:text-neutral-400">
+               {entry.createdBy}
+            </span>
             <span>{entry.body}</span>
          </p>
       </div>
