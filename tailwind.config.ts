2e1b532094991d42729eee28a52267b735b4f8d8
Oliver Cederborg <hey@olivercederborg.com>
2024-03-24 16:44:46 +0100

    feat(tailwind): add typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import typography from "@tailwindcss/typography"
 
 const config = {
    darkMode: ["class"],
@@ -74,7 +75,7 @@ const config = {
          },
       },
    },
-   plugins: [require("tailwindcss-animate")],
+   plugins: [require("tailwindcss-animate"), typography],
 } satisfies Config
 
 export default config
