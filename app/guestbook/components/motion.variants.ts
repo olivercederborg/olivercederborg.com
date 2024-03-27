e9d7544d6ade547f2c942ad93b42a04433512adc
Oliver Cederborg <hey@olivercederborg.com>
2024-03-27 04:50:05 +0100

    feat(motion): add default variants with no delay

--- app/guestbook/components/motion.variants.ts
+++ app/guestbook/components/motion.variants.ts
@@ -8,3 +8,12 @@ export const defaultVariants: Variants = {
       transition: { duration: 0.35, delay: 0.1, ease: "backOut" },
    },
 }
+
+export const defaultVariantsNoDelay: Variants = {
+   hidden: { opacity: 0, y: 20 },
+   visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.35, ease: "backOut" },
+   },
+}
