763e16bf5ef6737cffcb9953fd466e78078cbd8c
Oliver Cederborg <hey@olivercederborg.com>
2024-03-28 03:29:26 +0100

    feat(footer): update styling

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -3,15 +3,15 @@ import { Github } from "lucide-react"
 export function Footer() {
    return (
       <footer className="flex items-center justify-between border-t border-neutral-200 py-4 dark:border-neutral-900">
-         <p className="font-light text-neutral-500">
+         <p className="text-sm font-light text-neutral-600 dark:text-neutral-400">
             © {new Date().getFullYear()}
          </p>
          <a
             href="https://github.com/olivercederborg/olivercederborg.com"
             target="_blank"
-            className="flex items-center gap-2 border-b border-transparent text-neutral-900 hover:border-orange-300 dark:text-neutral-50"
+            className="flex items-center gap-2 text-sm text-neutral-700 hover:opacity-75 dark:text-neutral-50"
          >
-            <Github size={18} /> olivercederborg.com
+            <Github size={14} /> olivercederborg.com
          </a>
       </footer>
    )
