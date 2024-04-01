0d23dd520235e1e3471766d425ac463953e1512c
Oliver Cederborg <hey@olivercederborg.com>
2024-04-01 23:18:27 +0200

    feat: update navbar

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import * as React from "react"
 import { type NavItem } from "@/components/layouts/header"
 import { MobileNavigation } from "@/components/mobile-navigation"
 import { ThemeToggle } from "@/components/theme-toggle"
@@ -33,11 +34,11 @@ export const FloatingNav = ({
             ease: "backOut",
          }}
          className={cn(
-            "fixed inset-x-4 top-5 z-[49] mx-auto flex max-w-2xl items-center justify-between rounded-full border border-neutral-950 border-opacity-[0.03] bg-neutral-50/75 px-8 py-3 filter backdrop-blur-sm dark:border-white/5 dark:bg-neutral-900/75",
+            "fixed inset-x-4 top-5 z-[49] mx-auto flex max-w-2xl items-center justify-between overflow-hidden rounded-full border border-neutral-950 border-opacity-[0.03] bg-neutral-50/75 px-3 py-2 filter backdrop-blur-xl dark:border-white/5 dark:bg-neutral-900/75",
             className,
          )}
       >
-         <div className="hidden space-x-6 md:flex">
+         <div className="hidden md:flex">
             {navItems.map((navItem, idx: number) => {
                const isActive = pathname === navItem.link
                return (
@@ -45,23 +46,40 @@ export const FloatingNav = ({
                      key={`link=${idx}`}
                      href={navItem.link}
                      className={cn(
-                        "relative flex items-center space-x-1 text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300",
+                        "relative flex h-8 items-center px-4 text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300",
                      )}
                   >
-                     <span className="block sm:hidden">{navItem.icon}</span>
-                     <span className="hidden text-sm sm:block">
-                        <span className="text-neutral-400">/</span>
+                     <span
+                        className={cn("hidden text-sm sm:block")}
+                        style={{
+                           textShadow: isActive
+                              ? "1px 1px 12px rgba(255,255,255,0.4)"
+                              : "",
+                        }}
+                     >
+                        <span className="mr-px text-neutral-400">/</span>
                         {navItem.name.toLowerCase()}
                      </span>
+
                      {isActive && (
-                        <motion.span
-                           transition={{
-                              ease: "backInOut",
-                              duration: 0.35,
-                           }}
-                           layoutId="active-nav"
-                           className="absolute -inset-x-4 inset-y-auto z-[-1] hidden h-9 rounded-full bg-neutral-200/50 dark:bg-neutral-800 sm:flex"
-                        />
+                        <>
+                           <motion.span
+                              transition={{
+                                 ease: "backInOut",
+                                 duration: 0.45,
+                              }}
+                              layoutId="active-nav-glow"
+                              className="absolute inset-x-0 top-0 mx-auto hidden h-16 w-10 rounded-full bg-neutral-200/50 blur-[32px] dark:bg-white/75 sm:flex"
+                           />
+                           <motion.span
+                              transition={{
+                                 ease: "backInOut",
+                                 duration: 0.35,
+                              }}
+                              layoutId="active-nav"
+                              className="absolute inset-x-0 inset-y-0 z-[-1] hidden rounded-full bg-neutral-200/50 dark:bg-neutral-700 sm:flex"
+                           />
+                        </>
                      )}
                   </Link>
                )
